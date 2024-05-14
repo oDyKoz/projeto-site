9deea027464208a260c8d433042586b8548a4bb3
Dymas F. Antunes Ribeiro d. Santos <124197980+oDyKoz@users.noreply.github.com>
2024-05-13 21:41:44 -0300

    comment

--- Teste/main.js
+++ Teste/main.js
@@ -1,5 +1,10 @@
+// Adds an event listener to the document object that listens for a 'click' event
 document.addEventListener('click', function (evento) {
+  
+  // Checks if the target element of the event is not contained within the 'menu' element
   if (!menu.contains(evento.target)) {
+    
+    // Calls the 'ocultarMenu' function to hide the menu if the target element is not within the menu
     ocultarMenu();
   }
 });
